3be3d4e5ca0a526190ac98d38e1e3c0ef363ee39
Janvi <janvi@summachar.in>
2021-07-18 20:08:31 +0530

    signup and aboutus

--- resources/views/common/header.blade.php
+++ resources/views/common/header.blade.php
@@ -1,1 +1,14 @@
-<h1>header</h1>
+@section('styles')
+<link rel="stylesheet" href=".././css/header.css" />
+@endsection
+
+@section('body')
+<div class="top-bar">
+        <div class="menu first" >Home</div>
+        <div class="menu">About Us</div>
+        <div class="logo">
+         <img src="assets{{'signup/Logo.jpeg'}}" /></div>
+        <div class="menu">Login</div>
+        <div class="menu last" >Signup</div>
+    </div>
+@endsection
