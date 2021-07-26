6e1ac9326f8316d26f67f268fbf255e7700ae75e
Shalin Shah <sahilshh723@gmail.com>
2021-07-26 22:13:12 +0530

    Sign Up Screen

--- resources/views/authentication/signup.blade.php
+++ resources/views/authentication/signup.blade.php
@@ -1,22 +1,27 @@
 @extends('index')
 @section('title','Sign Up')
-    
+
 @section('styles')
     <link rel="stylesheet" href=".././css/signup.css" />
 @endsection
 
 @section('body')
    <div class="signup-div">
+       <div class="top-img">
+            <img class="l-first" src="{{'./images/signup/path 1.svg'}}" />
+       </div>
     <div class="l-div l-small">
-        <img class="l-first" src="assets{{'signup/signup-left.svg'}}" />
-        <img class="l-sec" src="./images/register.svg" />
+        <img class="l-first" src="{{'./images/signup/signup-left.svg'}}" />
+        <img class="l-sec" src="{{'./images/signup/register.svg'}}" />
     </div>
-    <div class="top-img">
-        <img src="./images/Path 2.svg" />
-    </div>
+
     <div class="r-div">
-        <img class="r-img" src=" assets{{'signup/path 2.svg'}}" />
+        <img class="r-img" src="{{'./images/signup/path 2.svg'}}" />
         <div class="form-column">
+
+            <div class="top-img">
+                <img class="l-sec" src="{{'./images/signup/register.svg'}}" />
+            </div>
 
             <div class="form-title">
                 <b>
@@ -25,32 +30,32 @@
             </div>
             <div class="form-fields">
                 <div class="input">
-                    <img src="assets{{'signup/user.svg'}}" />
+                    <img src="{{'./images/signup/user.svg'}}" />
                     <input type="text" placeholder="Username" />
                 </div>
                 <span class="err">Please fill this field!</span>
                 <div class="input">
-                    <img src="assets{{'signup/pen.svg'}}" />
+                    <img src="{{'./images/signup/pen.svg'}}" />
                     <input type="text" placeholder="Enrollment No." />
                 </div>
                 <span class="err">Please fill this field!</span>
                 <div class="input">
-                    <img src="assets{{'signup/email.svg'}}email.svg" />
+                    <img src="{{'./images/signup/email.svg'}}" />
                     <input type="text" placeholder="Email-id" />
                 </div>
                 <span class="err">Please fill this field!</span>
                 <div class="input">
-                    <img src="assets{{'signup/phone-call.svg'}}" />
-                    <input type="number" placeholder="Mobile-no" />
+                    <img src="{{'./images/signup/phone-call.svg'}}" />
+                    <input type="tel" placeholder="Mobile-no" />
                 </div>
                 <span class="err">Please fill this field!</span>
                 <div class="input">
-                    <img src="assets{{'signup/padlock.svg'}}padlock.svg" />
+                    <img src="{{'./images/signup/padlock.svg'}}" />
                     <input type="password" placeholder="Password" />
                 </div>
                 <span class="err">Please fill this field!</span>
                 <div class="input">
-                    <img src="assets{{'signup/confirm.svg'}}" />
+                    <img src="{{'./images/signup/confirm.svg'}}" />
                     <input type="password" placeholder="Confirm Password" />
                 </div>
                 <span class="err">Please fill this field!</span>
