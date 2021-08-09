af1b403822131a0d91def058b4ce050349ab90f5
Shalin Shah <sahilshh723@gmail.com>
2021-08-09 11:08:34 +0530

    Profile Page

--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -1,13 +1,65 @@
 @extends('common.main')
 
 @section('title',"Profile")
-@section('styles')
-    <link rel="stylesheet" href="" />
-@endsection
+
+@push('styles')
+    <link rel="stylesheet" href=".././css/profile.css" />
+@endpush
 
 @section('content')
-    <h1>Profile page</h1>
+    <div class="signup-div">
+        <div class="top-img">
+            <img class="l-first" src="{{'./images/signup/path 1.svg'}}" />
+        </div>
+        <div class="l-div l-small">
+            <img class="l-first" src="{{'./images/profile/Path189.svg'}}" />
+            <div class="avatar-parent">
+                <img class="avatar" src="{{'./images/profile/avatar.svg'}}" />
+                <img class="small-avatar" src="{{'./images/profile/avatar.svg'}}" />
+            </div>
+        </div>
+
+        <div class="r-div">
+            <div class="form-column">
+                <div class="top-img">
+                    <img class="l-sec" src="{{'./images/profile/avatar.svg'}}" />
+                </div>
+
+                <div class="form-fields">
+                    <div class="input">
+                        <input type="text" placeholder="Name" />
+                    </div>
+
+                    <div class="input">
+                        <input type="text" placeholder="Email-id" />
+                        <img src="{{'./images/signup/pen.svg'}}" />
+                    </div>
+                    <span class="err">Please fill this field!</span>
+                    <div class="input">
+                        <input type="tel" placeholder="Mobile-no" />
+                        <img src="{{'./images/signup/pen.svg'}}" />
+                    </div>
+                    <span class="err">Please fill this field!</span>
+                    <div class="input">
+                        <input type="text" placeholder="Enrollment No." />
+                    </div>
+
+
+                    <div class="input">
+                        <input type="text" placeholder="Department" />
+                    </div>
+
+                    <div class="input">
+
+                        <input type="text" placeholder="Institute" />
+                    </div>
+
+                    <button>
+                        Save
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
-@section('scripts')
-    <script src=""></script>
-@endsection
+
